0cf9f1f8183a3b6278894aeae4c1ea95d71459e1
amao <706894232@qq.com>
2020-06-09 18:07:28 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,37 @@
+"""
+print("你好，阿毛酱")  # 字符串
+print(2222)  #整数
+print(2.3333) #小数
+print(True) #布尔值
+print(()) #元祖
+print([]) #数组
+print({})  #字典
+
+print("哈哈",233,2.33)
+print("哈哈哈"*10)
+print("哈哈"+"嘻嘻")
+print((52+2)*5)
+print(2<3)
+
+
+a=input("请输入内容:")
+b=input("请继续输入内容：")
+print("获取内容",a+b)
+
+"""
+#数据格式的转换
+# print(type("哈哈"))
+# print(type(123))
+# print(type(1.23))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a=int(input("请输入"))
+# b=int(input("请继续输入"))
+# print("结果为",a+b)
+
+a=str(input('请输入'))
+b=str(input('继续输入'))
+print('长度为',len(a+b))
